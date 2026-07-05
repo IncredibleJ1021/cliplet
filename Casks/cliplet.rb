4f968a170385c83bce4e3150fae211ce78a108ee
IncredibleJ1021 <xjj2405907414@163.com>
2026-07-05 22:51:52 +0800

    Update Homebrew cask to v0.3.1

--- Casks/cliplet.rb
+++ Casks/cliplet.rb
@@ -1,6 +1,6 @@
 cask "cliplet" do
-  version "0.3.0"
-  sha256 "5152debb278e43243dabfb3d9e90028dfff973c60385665823d411f48fa20ec6"
+  version "0.3.1"
+  sha256 "12786e6310b1a197963a3b8b03d0e31f8f7f1fb32b36740c5306829f3e2b97f8"
 
   url "https://github.com/IncredibleJ1021/cliplet/releases/download/v#{version}/cliplet-macOS-v#{version}.zip"
   name "cliplet"
